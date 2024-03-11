fadde22a2a801aee22fc2b9fd6258436ff71ed40
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-03-12 00:10:58 +0700

    + add svg-icon ,definition components, ref, img src to svg
    + assets svg

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from "@angular/core";
 import { HeaderComponent } from "./components/header/header.component";
 import { SideNavComponent } from "./components/sidenav/sidenav.component";
+import { SvgDefinitionsComponent } from "./components/svg-definitions/svg-definitions.component";
+import { SvgIcon } from "./components/svg-icon/svg-icon.component";
 
 @NgModule({
     declarations: [
         HeaderComponent,
-        SideNavComponent
+        SideNavComponent,
+        SvgDefinitionsComponent,
+        SvgIcon
     ],
     imports: [],
     exports: [
         HeaderComponent,
-        SideNavComponent
+        SideNavComponent,
+        SvgDefinitionsComponent,
+        SvgIcon
     ]
 })
 export class CoreModule {
