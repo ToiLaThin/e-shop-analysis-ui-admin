c603e969ed5e1711b003379f633cfa769727a1b2
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-03-20 00:43:11 +0700

    + change svg to have fill color, style dropdown in header component

--- src/app/core/components/svg-icon/svg-icon.component.ts
+++ src/app/core/components/svg-icon/svg-icon.component.ts
@@ -9,6 +9,7 @@ export class SvgIcon {
     @Input() wSizeIsHSize: boolean = true;
     @Input() hSize: number = 16; // if wSizeIsHSize == false, you must provide hSize
     @Input() iconName!: string;
+    @Input() currentColor = 'currentColor'
     get iconUrl() {
         return `${window.location.href}#${this.iconName}`
         //co window.location.href cho chac an, #iconName da du
