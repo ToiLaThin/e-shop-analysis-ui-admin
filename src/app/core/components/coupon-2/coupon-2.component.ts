e7269527b90626f4607b010cf8e0db2d296275d0
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-03-28 00:19:48 +0700

    + add and style product card, chip, coupon components

---
+++ src/app/core/components/coupon-2/coupon-2.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+import { DiscountType } from '../../ui-models/discount-type';
+
+@Component({
+    selector: 'app-coupon-2',
+    templateUrl: './coupon-2.component.html',
+    styleUrls: ['./coupon-2.component.scss']
+})
+export class Coupon2Component {
+}
---
+++ src/app/core/components/product-chip-3/product-chip-3.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-product-chip-3',
+    templateUrl: './product-chip-3.component.html',
+    styleUrls: ['./product-chip-3.component.scss']
+})
+export class ProductChip3Component {
+
+}
